43df2371dd12052474693e7859b456790eb00a97
orontamir <orontamir@gmail.com>
2021-12-14 22:34:54 +0200

    add configuration to appsettings

--- Aquariusengines/Controllers/HomeController.cs
+++ Aquariusengines/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Aquariusengines.ViewModels;
 using DataModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 
 namespace Aquariusengines.Controllers
@@ -10,26 +11,15 @@ namespace Aquariusengines.Controllers
     public class HomeController : Controller
     {
         private readonly ISignal _signal;
-
-        public HomeController(ISignal signal)
+        private readonly IConfiguration _configuration;
+        public HomeController(ISignal signal, IConfiguration configuration)
         {
             _signal = signal;
-            
+            _configuration = configuration;
         }
+
         // GET: Home
         public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Home/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Home/Create
-        public ActionResult Create()
         {
             return View();
         }
@@ -40,6 +30,10 @@ namespace Aquariusengines.Controllers
         {
             try
             {
+                //get configuration
+                double amplitude = double.Parse(_configuration["amplitude"]);
+                double max_range = double.Parse(_configuration["max_range"]);
+                double min_range = double.Parse(_configuration["min_range"]); 
                 //Check Sine Validation
                 Sine _sine = new Sine();
 
@@ -50,7 +44,7 @@ namespace Aquariusengines.Controllers
                     Time = ((DateTime)(collection["Time"])).TimeOfDay.ToString(),
                     Date = ((DateTime)(collection["Time"])).Date.ToString(),
                     Value = (double)collection["Sine"],
-                    Error = _sine.Validation((double)collection["Sine"],0.0,32.0)
+                    Error = _sine.Validation((double)collection["Sine"],0.0, amplitude)
                 };
                 //Insert Sine to Data base
                 _signal.AddSignal(sine);
@@ -64,7 +58,7 @@ namespace Aquariusengines.Controllers
                     Time = ((DateTime)(collection["Time"])).TimeOfDay.ToString(),
                     Date = ((DateTime)(collection["Time"])).Date.ToString(),
                     Value = (double)collection["State"],
-                    Error = _state.Validation((double)collection["State"], 256, 4095)
+                    Error = _state.Validation((double)collection["State"], min_range, max_range)
                 };
                 //Insert State to Data Base
                 _signal.AddSignal(state);
@@ -76,28 +70,7 @@ namespace Aquariusengines.Controllers
             }
         }
 
-        // GET: Home/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Home/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+      
 
        
     }
